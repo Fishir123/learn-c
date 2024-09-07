b6950062d89f4713c815d98536ec4a350a559993
Fishir123 <faiqthestrongest@gmail.com>
2024-09-07 14:12:40 +0700

    menambahkan perulangan(looping)

---
+++ pengulangan(loop)/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+void main()
+{
+    //perulangan for
+    /*for(int c = 0; c < 10; c++){
+        printf("pengulangan ke-%i\n", c);
+    }*/
+
+    /*for (int counter = 0; counter < 50; counter+=2){
+        printf("Perulangan ke - %i\n", counter);
+    }*/
+
+    /*for(int counter = 10; counter >= 0; counter--){
+        printf("Perulangan ke - %i\n", counter);
+    }*/
+
+    //perulangan while
+    /*char ulangi = 'y';
+    int counter = 0;
+
+    while(ulangi == 'y'){
+        printf("Apakah kamu mau mengulang?\n");
+        printf("Jawab (y/t): ");
+        scanf(" %c", &ulangi);
+
+        //increment counter
+        counter++;
+    }
+
+    printf("\n\n----------\n");
+    printf("Perulangan Selesai!\n");
+    printf("Kamu mengulang sebanyak %i kali.\n", counter);*/
+
+    //perulangan do while
+
+    /*char ulangi = 'y';
+    int counter = 0;
+
+    do {
+        printf("Apakah kamu mau mengulang?\n");
+        printf("Jawab (y/t): ");
+        scanf(" %c", &ulangi);
+
+        //increment counter
+        counter++;
+    } while (ulangi == 'y');
+
+    printf("\n\n------------\n");
+    printf("Perulangan Selesai!\n");
+    printf("Kamu mengulang sebanyak %i kali.\n", counter);*/
+
+    //perulangan beresarang/nested loop
+    /*for (int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            printf("Perulangan ke (%d, %d)\n", i, j);
+        }
+    }*/
+
+}
+
+
